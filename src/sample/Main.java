9c3ded1ad37619dfaec96a70c2424113858f3164
U1810291 <otash158@bk.ru>
2019-12-16 12:17:00 +0500

    After some changes

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,37 +6,42 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+//It has some errors make sure that I resolved it before running this code
+//    @Override
+//    public void start(Stage primaryStage) throws Exception{
+//        Parent root = FXMLLoader.load(getClass().getResource("/sample/View/EditsViews/Librarians.fxml"));
+//        primaryStage.setTitle("Librarians List");
+//        primaryStage.setScene(new Scene(root, 600, 400));
+//        primaryStage.show();
+//    }
+//Until here
+
+
+
+
 
 //    @Override
 //    public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("Register/Register.fxml"));
-//        primaryStage.setTitle("Hello World");
+//        Parent root = FXMLLoader.load(getClass().getResource("View/RegisterView/Login.fxml"));
+//        primaryStage.setTitle("Login");
 //        primaryStage.setScene(new Scene(root, 600, 400));
 //        primaryStage.show();
 //    }
 
+//    @Override
+//    public void start(Stage primaryStage) throws Exception{
+//        Parent root = FXMLLoader.load(getClass().getResource("/sample/View/EditsViews/Students.fxml"));
+//        primaryStage.setTitle("Students list");
+//        primaryStage.setScene(new Scene(root, 600, 400));
+//        primaryStage.show();
+//    }
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("View/RegisterView/LoginController.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("/sample/View/EditsViews/StudentEdit.fxml"));
+        primaryStage.setTitle("Student Edit");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
-
-//    @Override
-//    public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("Librarian_student/Students.fxml"));
-//        primaryStage.setTitle("Hello World");
-//        primaryStage.setScene(new Scene(root, 600, 400));
-//        primaryStage.show();
-//    }
-//    @Override
-//    public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("Librarian_student/Students.fxml"));
-//        primaryStage.setTitle("Hello World");
-//        primaryStage.setScene(new Scene(root, 600, 400));
-//        primaryStage.show();
-//    }
 //    @Override
 //    public void start(Stage primaryStage) throws Exception{
 //        Parent root = FXMLLoader.load(getClass().getResource("Librarian/Librarian_student/Students.fxml"));
@@ -46,8 +51,8 @@ public class Main extends Application {
 //    }
 //    @Override
 //    public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("Register/Register.fxml"));
-//        primaryStage.setTitle("Hello World");
+//        Parent root = FXMLLoader.load(getClass().getResource("/sample/View/RegisterView/Register.fxml"));
+//        primaryStage.setTitle("Registration");
 //        primaryStage.setScene(new Scene(root, 600, 400));
 //        primaryStage.show();
 //    }
